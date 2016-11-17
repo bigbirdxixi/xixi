eac71de6c709e455ec15acd000c0e6e4e633f0d9
bigbirdxixi <bigbirdxixi@gmail.com>
2016-11-17 21:53:59 +0800

    new py files

---
+++ check_local_service.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python2
+# -*- coding:utf-8 -*-
+import commands,sys
+from optparse import OptionParser
+
+parser = OptionParser(usage="%prog -w <warning threshold> -c <critical threshold> [ -h ]\n\n",version="%prog ")
+parser.add_option("-s", "--service",action="store", type="string",dest="service", help="service name")
+parser.add_option("-w", "--waring",action="store", type="int",dest="waring", help="Thrift connect waring line")
+parser.add_option("-c", "--critical",action="store", type="int",dest="critical", help="Thrift connect critical line")
+(options, args) = parser.parse_args()
+
+def serstatu( sername , warings = 800 , criticals = 1000 ):
+  if options.waring != None:
+    warings = options.waring
+
+  if options.critical != None:
+    criticals = options.critical
+
+  thepid = str(commands.getoutput("source /etc/profile && jps -l|grep -v grep | grep %s | awk '{print $1}'" %sername))
+  if thepid == "":
+    print("ERROR,the %s service is down" %sername)
+    sys.exit(2)
+  else:
+    thecon = int(commands.getoutput("netstat -antp | grep -v grep | grep -v LISTEN | grep %s | wc -l" %thepid))
+    if 0 < thecon < warings:
+      print("OK , the %s pid:%s connect %d | thecon=%d;%d;%d;0" %(sername,thepid,thecon,thecon,warings,criticals))
+      sys.exit(0)
+    elif warings <= thecon < criticals:
+      print("WARING , the %s pid:%s connect %d | thecon=%d;%d;%d;0" %(sername,thepid,thecon,thecon,warings,criticals))
+      sys.exit(1)
+    elif thecon >= criticals:
+      print("CRITICAL , the %s pid:%s connect %d | thecon=%d;%d;%d;0" %(sername,thepid,thecon,thecon,warings,criticals))
+      sys.exit(2)
+    else:
+      print('UNKNOWN STATUS')
+      sys.exit(3)
+serstatu(options.service)
