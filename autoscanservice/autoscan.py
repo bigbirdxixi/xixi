c0cbfc450cb4b6fdc187fa762099ff27d1df6431
bigbirdxixi <bigbirdxixi@gmail.com>
2016-12-07 15:04:04 +0800

    add tomcat scan to autoscan.py

--- autoscanservice/autoscan.py
+++ autoscanservice/autoscan.py
@@ -12,7 +12,20 @@ def registser(host,port,sername):
   return bool(rel)
 
 #获取服务名
-s = commands.getoutput(r"sudo jps -l | grep -v grep | grep com. | awk '{print $2}'").split('\n')
+def getsername():
+  '获取服务名，返回服务名清单sername与tomcat站点端口对应字典a'
+  sername = commands.getoutput(r"sudo jps -l | grep -v grep | grep com. | awk '{print $2}'").split('\n')
+  portpid = commands.getoutput(r"sudo netstat -ntpl | grep -E '8\w{2}0' | awk -F ':|/' '{print $4,$(NF-1)}' | awk '{print $1,$4}'").split('\n')
+  a = {}
+  for n in portpid:
+    sitename = 'site_' + commands.getoutput(r"sudo ps -ef | grep -vE 'grep|autoscan.py' | grep %s | awk -F '/' '{print $(NF-1)}'" %n.split(' ')[1])
+    a[sitename] = {'siteport' : n.split(' ')[0], 'sitepid' : n.split(' ')[1]}
+    sername.append(sitename)
+  return sername,a
+
+s,sitemap = getsername()
+localip = socket.gethostbyaddr(socket.gethostname())[2][0]
+
 try:
   con = sqlite3.connect(r'./wsf.db')
   cur = con.cursor()
@@ -20,7 +33,12 @@ try:
   cur.execute(r'create table if not exists service (sername txt primary key)')
   for n in s:
     oldser = cur.execute(r'select sername from service where sername = ?',(n,)).fetchone()
-    if oldser == None:
+    if oldser == None and n.startswith('site_'):
+      cur.execute(r"insert into service (sername) values (?)",(n,))
+      os.system(r"echo 'command[%s]=/usr/local/nagios/libexec/check_http -I %s -p %s -w 5 -c 10' >> /usr/local/nagios/etc/nrpe.cfg" %(n,localip,sitemap[n]['siteport']))
+      registser('172.31.6.109',4420,n)
+      os.system(r"kill -9 `ps -ef | grep -v grep | grep nrpe | awk '{print $2}'` && /usr/local/nagios/bin/nrpe -c /usr/local/nagios/etc/nrpe.cfg -d")
+    elif oldser == None:
       cur.execute(r"insert into service (sername) values (?)",(n,))
       os.system(r"echo 'command[%s]=/usr/local/nagios/libexec/check_local_service.py -s %s' >> /usr/local/nagios/etc/nrpe.cfg" %(n,n))
       registser('172.31.6.109',4420,n)
